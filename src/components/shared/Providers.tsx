281d6cd22d0db2f75aa522c6964cdcb2fc5f943a
Kien Ngo <github@kien.ngo>
2024-06-05 02:07:40 -0400

    Dark theme by default

--- src/components/shared/Providers.tsx
+++ src/components/shared/Providers.tsx
@@ -5,25 +5,27 @@ import type { ReactNode } from "react";
 import { ThirdwebProvider } from "thirdweb/react";
 
 const theme = extendTheme({
-	components: {
-		// Accordion: {
-		// 	baseStyle: {
-		// 		container: {
-		// 			border: "none",
-		// 		},
-		// 		button: {
-		// 			// borderTop: "1px solid rgba(255,255,255,0.5)",
-		// 			borderBottom: "1px solid rgba(255,255,255,0.5)",
-		// 		},
-		// 	},
-		// },
-	},
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+  components: {
+    // Accordion: {
+    // 	baseStyle: {
+    // 		container: {
+    // 			border: "none",
+    // 		},
+    // 		button: {
+    // 			// borderTop: "1px solid rgba(255,255,255,0.5)",
+    // 			borderBottom: "1px solid rgba(255,255,255,0.5)",
+    // 		},
+    // 	},
+    // },
+  },
 });
 
 export function Providers({ children }: { children: ReactNode }) {
-	return (
-		<ChakraProvider theme={theme}>
-			<ThirdwebProvider>{children}</ThirdwebProvider>
-		</ChakraProvider>
-	);
+  return (
+    <ChakraProvider theme={theme}>
+      <ThirdwebProvider>{children}</ThirdwebProvider>
+    </ChakraProvider>
+  );
 }
