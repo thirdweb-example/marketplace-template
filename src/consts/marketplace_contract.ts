026379a2fac62478ba5d60793e5bb25473ba3e8a
Kien Ngo <github@kien.ngo>
2024-06-06 07:55:35 -0400

    Add support for ETH Sepolia

--- src/consts/marketplace_contract.ts
+++ src/consts/marketplace_contract.ts
@@ -1,5 +1,5 @@
 import type { Chain } from "thirdweb";
-import { avalancheFuji, polygonAmoy } from "./chains";
+import { avalancheFuji, polygonAmoy, sepolia } from "./chains";
 
 type MarketplaceContract = {
   address: string;
@@ -18,5 +18,9 @@ export const MARKETPLACE_CONTRACTS: MarketplaceContract[] = [
   {
     address: "0x571B773F1e4A7C080b51C36f37e06f371C515569",
     chain: polygonAmoy,
+  },
+  {
+    address: "0xe0eFD6fb388405b67b3E9FaFc02649c70E749f03",
+    chain: sepolia,
   },
 ];
