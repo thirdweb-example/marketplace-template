6510852bb855620d722f5bddf4972001626873b4
Kien Ngo <github@kien.ngo>
2024-06-11 18:36:24 -0400

    Fix contrast 2

--- src/components/shared/Providers.tsx
+++ src/components/shared/Providers.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { chakraTheme } from "@/consts/chakra";
-import { ChakraProvider } from "@chakra-ui/react";
+import { chakraTheme, chakraThemeConfig } from "@/consts/chakra";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
 import { ThirdwebProvider } from "thirdweb/react";
@@ -10,6 +10,7 @@ const queryClient = new QueryClient();
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ChakraProvider theme={chakraTheme}>
+      <ColorModeScript initialColorMode={chakraThemeConfig.initialColorMode} />
       <QueryClientProvider client={queryClient}>
         <ThirdwebProvider>{children}</ThirdwebProvider>
       </QueryClientProvider>
