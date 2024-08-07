caa1936dded682fdc7f7727e64ced8c7f9970776
IamSouthernFried <128635781+IamSouthernFried@users.noreply.github.com>
2024-08-06 22:04:14 -0500

    Update nft_contracts.ts

--- src/consts/nft_contracts.ts
+++ src/consts/nft_contracts.ts
@@ -1,5 +1,5 @@
 import type { Chain } from "thirdweb";
-import { yourCustomChain, eth, bsc } from "./chains";
+import { yourCustomChain } from "./chains";
 
 export type NftContract = {
   address: string;
@@ -27,49 +27,6 @@ export const NFT_CONTRACTS: NftContract[] = [
       "https://d391b93f5f62d9c15f67142e43841acc.ipfscdn.io/ipfs/bafybeichanaklyw6ogjtxtnh4yums4bjflbzvlnnrtorlrl4gddrakpnia/download%20(16).png",
     type: "ERC721",
   },
-  {
-    address: "0xC5A2c72c581eA4A17e17bEeF38a9597132830401",
-    chain: avalancheFuji,
-    title: "Ugly Waifu",
-    thumbnailUrl:
-      "https://258c828e8cc853bf5e0efd001055fb39.ipfscdn.io/ipfs/bafybeidaadqapi7twzd7pjp24tu4ngsr3teubrhop7hg3jk3oj6lqysfgm/OS-LOGO.png",
-    slug: "ugly-waifu",
-    type: "ERC721",
-  },
-
-  {
-    address: "0x0896Db00D8987Fba2152aa7c14c4255eBC7354cE",
-    chain: avalancheFuji,
-    title: "Unnamed Collection",
-    description: "",
-    thumbnailUrl:
-      "https://258c828e8cc853bf5e0efd001055fb39.ipfscdn.io/ipfs/Qmct2vS78Uwug3zVtqQognskPPRmd4wRQiaDAQWt1kRJws/0.png",
-    slug: "unnamed-collection",
-    type: "ERC721",
-  },
-  {
-    address: "0x0ACaCa3d3F64bb6e6D3564BBc891c58Bd4A4c83c",
-    chain: avalancheFuji,
-    title: "GoroBot",
-    thumbnailUrl:
-      "https://258c828e8cc853bf5e0efd001055fb39.ipfscdn.io/ipfs/bafybeiay3ffxy3os56bvnu5cmq7gids4v6n4hf5nvvcb3gy2dzavi3ltnu/profile.jpg",
-    slug: "gorobot",
-    type: "ERC721",
-  },
-  {
-    address: "0x4b6CDEFF5885A57678261bb95250aC43aD490752",
-    chain: polygonAmoy,
-    title: "Mata NFT",
-    thumbnailUrl:
-      "https://258c828e8cc853bf5e0efd001055fb39.ipfscdn.io/ipfs/bafybeidec7x6bptqmrxgptaedd7wfwxbsccqfogzwfsd4a7duxn4sdmnxy/0.png",
-    type: "ERC721",
-  },
-  {
-    address: "0xd5e815241882676F772A624E3892b27Ff3a449c4",
-    chain: avalancheFuji,
-    title: "Cats (ERC1155)",
-    thumbnailUrl:
-      "https://258c828e8cc853bf5e0efd001055fb39.ipfscdn.io/ipfs/bafybeif2nz6wbwuryijk2c4ayypocibexdeirlvmciqjyvlzz46mzoirtm/0.png",
-    type: "ERC1155",
-  },
+ 
+ 
 ];
