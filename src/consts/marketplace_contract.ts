7ec31f0e41ac1d6dfacd93a0e1801a1728e1b360
IamSouthernFried <128635781+IamSouthernFried@users.noreply.github.com>
2024-08-06 22:03:20 -0500

    Update marketplace_contract.ts

--- src/consts/marketplace_contract.ts
+++ src/consts/marketplace_contract.ts
@@ -1,5 +1,5 @@
 import type { Chain } from "thirdweb";
-import {  ethereum, bsc, yourCustomChain } from "./chains";
+import {  yourCustomChain } from "./chains";
 
 type MarketplaceContract = {
   address: string;
@@ -15,12 +15,6 @@ export const MARKETPLACE_CONTRACTS: MarketplaceContract[] = [
     address: "0x952e803BB045A4Cf9CDbE9AB1281B065fd07Eb76",
     chain: yourCustomChain,
   },
-  {
-    address: "0x571B773F1e4A7C080b51C36f37e06f371C515569",
-    chain: polygonAmoy,
-  },
-  {
-    address: "0xe0eFD6fb388405b67b3E9FaFc02649c70E749f03",
-    chain: sepolia,
-  },
+ 
+
 ];
