0a8ccbf1d1c9883fff06d3cfff7134f53c6b5084
IamSouthernFried <128635781+IamSouthernFried@users.noreply.github.com>
2024-08-07 12:53:10 -0500

    Update nft_contracts.ts

--- src/consts/nft_contracts.ts
+++ src/consts/nft_contracts.ts
@@ -27,6 +27,13 @@ export const NFT_CONTRACTS: NftContract[] = [
       "https://d391b93f5f62d9c15f67142e43841acc.ipfscdn.io/ipfs/bafybeichanaklyw6ogjtxtnh4yums4bjflbzvlnnrtorlrl4gddrakpnia/download%20(16).png",
     type: "ERC721",
   },
+  {
+    address:"0xccf0dF476C15f7e9e554d8d549cF8ccc34512afF",
+    chain:yourCustomChain,
+    title:"Random Collection",
+    thumbnailUrl:"https://d391b93f5f62d9c15f67142e43841acc.ipfscdn.io/ipfs/bafybeigx3ybgrpriickkb4memqtr27zgl52p6k6fpbzaasxzgnpvh5tfmi/southernfried.nfts_two_sheep_with_afro_hair_sitting_on_the_gras_fa1ac42a-cb61-4b5a-a65b-ee7dfac33282.png",
+    type:"ERC721",
+  },
  
  
 ];
