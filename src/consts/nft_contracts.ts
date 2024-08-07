38503e05ddafa5cfda4a6f972cb5fd99a1c6b63b
IamSouthernFried <128635781+IamSouthernFried@users.noreply.github.com>
2024-08-07 13:43:20 -0500

    Update nft_contracts.ts

--- src/consts/nft_contracts.ts
+++ src/consts/nft_contracts.ts
@@ -8,7 +8,6 @@ export type NftContract = {
 
   title?: string;
   description?: string;
-  thumbnailUrl?: string;
   slug?: string;
 };
 
@@ -23,16 +22,19 @@ export const NFT_CONTRACTS: NftContract[] = [
     address: "0xe2Bc16894eeedE77915faA297047A08a5073C55C",
     chain: yourCustomChain,
     title: "Test Collection",
-    thumbnailUrl:
-      "https://d391b93f5f62d9c15f67142e43841acc.ipfscdn.io/ipfs/bafybeichanaklyw6ogjtxtnh4yums4bjflbzvlnnrtorlrl4gddrakpnia/download%20(16).png",
     type: "ERC721",
   },
   {
     address:"0xccf0dF476C15f7e9e554d8d549cF8ccc34512afF",
     chain:yourCustomChain,
     title:"Random Collection",
-    thumbnailUrl:"https://d391b93f5f62d9c15f67142e43841acc.ipfscdn.io/ipfs/bafybeigx3ybgrpriickkb4memqtr27zgl52p6k6fpbzaasxzgnpvh5tfmi/southernfried.nfts_two_sheep_with_afro_hair_sitting_on_the_gras_fa1ac42a-cb61-4b5a-a65b-ee7dfac33282.png",
     type:"ERC721",
+  },
+  {
+    address:"0x72a7De5e5678126A1Cb42cC86052b00027D933ae",
+    chain:yourCustomChain,
+    title:"my collection",
+    type:ERC721",
   },
  
  
