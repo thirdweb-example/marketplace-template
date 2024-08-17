deaee32cdfa2532d4d115e6a0c335f26b12b6d67
IamSouthernFried <128635781+IamSouthernFried@users.noreply.github.com>
2024-08-17 13:55:17 -0500

    Update nft_contracts.ts

--- src/consts/nft_contracts.ts
+++ src/consts/nft_contracts.ts
@@ -19,26 +19,12 @@ export type NftContract = {
  */
 export const NFT_CONTRACTS: NftContract[] = [
   {
-    address: "0xe2Bc16894eeedE77915faA297047A08a5073C55C",
+    address: "0x789825ddaA3C41ef2876B6F152996681B6242253",
     chain: yourCustomChain,
-    title: "Test Collection",
+    title: "The National Fentanyl Awareness Day Charity 2024",
     type: "ERC721",
     thumbnailUrl: "",
   },
-  {
-    address:"0xccf0dF476C15f7e9e554d8d549cF8ccc34512afF",
-    chain:yourCustomChain,
-    title:"Random Collection",
-    type:"ERC721",
-    thumbnailUrl:"",
-  },
-  {
-    address:"0x72a7De5e5678126A1Cb42cC86052b00027D933ae",
-    chain:yourCustomChain,
-    title:"my collection",
-    type:"ERC721",
-    thumbnailUrl:"",
-  },
- 
- 
+
+
 ];
