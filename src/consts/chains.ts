9c462dd201474fcfb31b63964164ea3fc24d8f23
IamSouthernFried <128635781+IamSouthernFried@users.noreply.github.com>
2024-08-17 13:51:31 -0500

    Update chains.ts

--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -1,7 +1,7 @@
 // chain.ts
 import { defineChain } from "thirdweb/chains";
 
-const yourChainId = 14333;
+const yourChainId = 1490;
 const yourCustomChain = defineChain(yourChainId);
 
 export { yourCustomChain }
