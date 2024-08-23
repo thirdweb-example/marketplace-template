c126d7df175f836c6696e642b48ed02b40d37172
IamSouthernFried <128635781+IamSouthernFried@users.noreply.github.com>
2024-08-23 14:30:34 -0500

    Delete src/components/token-page/BuyFromListing.tsx

--- src/components/token-page/BuyFromListing.tsx
+++
@@ -1,9 +0,0 @@
-import { prepareContractCall, sendTransaction } from "thirdweb";
-
-const transaction = await prepareContractCall({ 
-  contract, const contract = someValue}); // Assign a value to contract
-const transaction = await prepareContractCall({ contract, method: "function buyFromListing(uint256 _listingId, address _buyFor, uint256 _quantity, ad...", params: [_listingId, _buyFor, _quantity, _currency, _exp });
-const { transactionHash } = await sendTransaction({ 
-  transaction, 
-  account 
-});
