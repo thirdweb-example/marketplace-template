e5d7ea2c131196d9b4cb9d66f6c3eddac04bd133
Kien Ngo <github@kien.ngo>
2024-09-25 23:45:14 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
 import type { Metadata } from "next";
 import { Providers } from "@/components/shared/Providers";
 import { Navbar } from "@/components/shared/Navbar";
+import { AutoConnect } from "thirdweb/react";
+import { client } from "@/consts/client";
 
 export const metadata: Metadata = {
-  title: "Marketplace",
-  description: "",
+	title: "Marketplace",
+	description: "",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body style={{ paddingBottom: "100px" }}>
-        <Providers>
-          <Navbar />
-          {children}
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body style={{ paddingBottom: "100px" }}>
+				<Providers>
+					<AutoConnect client={client} />
+					<Navbar />
+					{children}
+				</Providers>
+			</body>
+		</html>
+	);
 }
